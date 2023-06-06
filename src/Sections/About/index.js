a0d930b917d52a50f2a4c209c117ba2b01459e48
Hamid Nazari <nhamid.info@gmail.com>
2023-06-06 18:35:55 +0200

    CorigerCorriger les fautes et changement de CV.

--- src/Sections/About/index.js
+++ src/Sections/About/index.js
@@ -3,54 +3,55 @@ import hand from '../../assets/hand.svg';
 import rocket from '../../assets/rocket.png';
 import about from '../../assets/about.svg';
 import {
-    AboutSection,
-    Waves,
-    Hand,
-    Main,
-    Title,
-    CurvedLine,
-    Content,
-    Rocket,
-    AboutText,
-    About,
-    Text,
-    Circle,
+  AboutSection,
+  Waves,
+  Hand,
+  Main,
+  Title,
+  CurvedLine,
+  Content,
+  Rocket,
+  AboutText,
+  About,
+  Text,
+  Circle,
 } from '../About/AboutElements';
 
 const index = () => {
-    return (
-        <AboutSection id="about">
-            <Waves src={waves} alt="Waves" width="400" height="400" />
-            <Hand>
-                <img src={hand} alt="Hand" width="400" height="400" />
-            </Hand>
-            <Main>
-                <div>
-                    <Title>À propos moi</Title>
-                    <CurvedLine />
-                </div>
-                <Content>
-                    <Rocket src={rocket} alt="Rocket" width="400" height="400" />
-                    <AboutText>
-                        <About>
-                            <img src={about} alt="About" width="400" height="400" />
-                        </About>
-                        <Text>
-                            Je suis un développeur full stack, passionné de développement et design qui peut fournir un
-                            code propre et une conception parfaite au pixel près, je rends également le site web de plus
-                            en plus interactif avec une animation web. Aujourd'hui, je suis disponible pour des nouveaux
-                            opportunités et j'aime des challenges.
-                        </Text>
-                        <div>
-                            <Circle style={{ backgroundColor: 'var(--purple)' }} />
-                            <Circle style={{ backgroundColor: 'var(--pink)' }} />
-                            <Circle style={{ backgroundColor: 'var(--black)' }} />
-                        </div>
-                    </AboutText>
-                </Content>
-            </Main>
-        </AboutSection>
-    );
+  return (
+    <AboutSection id='about'>
+      <Waves src={waves} alt='Waves' width='400' height='400' />
+      <Hand>
+        <img src={hand} alt='Hand' width='400' height='400' />
+      </Hand>
+      <Main>
+        <div>
+          <Title>À propos moi</Title>
+          <CurvedLine />
+        </div>
+        <Content>
+          <Rocket src={rocket} alt='Rocket' width='400' height='400' />
+          <AboutText>
+            <About>
+              <img src={about} alt='About' width='400' height='400' />
+            </About>
+            <Text>
+              Je suis un développeur full stack, passionné de développement et
+              design qui peut fournir un code propre et une conception parfaite
+              au pixel près, je rends également le site web de plus en plus
+              interactif avec une animation web. Aujourd'hui, je suis disponible
+              pour des nouvelles opportunités et j'aime des challenges.
+            </Text>
+            <div>
+              <Circle style={{ backgroundColor: 'var(--purple)' }} />
+              <Circle style={{ backgroundColor: 'var(--pink)' }} />
+              <Circle style={{ backgroundColor: 'var(--black)' }} />
+            </div>
+          </AboutText>
+        </Content>
+      </Main>
+    </AboutSection>
+  );
 };
 
 export default index;
--- src/Sections/Hero/index.js
+++ src/Sections/Hero/index.js
@@ -2,60 +2,60 @@ import { AiOutlineCloudDownload } from 'react-icons/ai';
 import Typical from 'react-typical';
 import Dev from '../../assets/dev.svg';
 import {
-    HeroSection,
-    Blobs,
-    MainContent,
-    LeftBlock,
-    Topic,
-    Title,
-    SubText,
-    Circle,
-    CTA,
-    DevSvg,
+  HeroSection,
+  Blobs,
+  MainContent,
+  LeftBlock,
+  Topic,
+  Title,
+  SubText,
+  Circle,
+  CTA,
+  DevSvg,
 } from '../Hero/HeroElements';
 
 const index = () => {
-    return (
-        <HeroSection id="home">
-            <Blobs></Blobs>
-            <MainContent>
-                <LeftBlock>
-                    <Topic>
-                        <Circle />
-                        <span>
-                            <h1>
-                                <Typical
-                                    steps={[
-                                        'Ethousiaste Développeur 🔴',
-                                        1000,
-                                        'Full Stack Développeur 💻',
-                                        1000,
-                                        'Mern Stack Développeur 😎',
-                                        1000,
-                                        'Multiplateforme Développeur 🌐',
-                                        1000,
-                                        'React/React Native Développeur 📱',
-                                        1000,
-                                    ]}
-                                    loop={Infinity}
-                                    wrapper="p"
-                                />
-                            </h1>
-                        </span>
-                    </Topic>
-                    <Title>Bonjour, Je suis Hamid</Title>
-                    <SubText>Bienvenue dans mon univers</SubText>
-                    <a href="HN-CV.pdf" download="Hamid HN-CV.pdf">
-                        <CTA>
-                            Télécharger le CV &nbsp;
-                            <AiOutlineCloudDownload style={{ height: '3rem', width: '2rem' }} />
-                        </CTA>
-                    </a>
-                </LeftBlock>
-                <DevSvg src={Dev} alt="Dev SVG" width="400" height="400" />
-            </MainContent>
-        </HeroSection>
-    );
+  return (
+    <HeroSection id='home'>
+      <Blobs></Blobs>
+      <MainContent>
+        <LeftBlock>
+          <Topic>
+            <Circle />
+            <span>
+              <h1>
+                <Typical
+                  steps={[
+                    'Full Stack Développeur 💻',
+                    1000,
+                    'Mern Stack Développeur 😎',
+                    1000,
+                    'Multiplateforme Développeur 🌐',
+                    1000,
+                    'React/React Native Développeur 📱',
+                    1000,
+                  ]}
+                  loop={Infinity}
+                  wrapper='p'
+                />
+              </h1>
+            </span>
+          </Topic>
+          <Title>Bonjour, Je suis Hamid</Title>
+          <SubText>Bienvenue dans mon univers</SubText>
+          <a href='HN-CV.pdf' download='Hamid HN-CV.pdf'>
+            <CTA>
+              Télécharger le CV &nbsp;
+              <AiOutlineCloudDownload
+                style={{ height: '3rem', width: '2rem' }}
+              />
+            </CTA>
+          </a>
+        </LeftBlock>
+        <DevSvg src={Dev} alt='Dev SVG' width='400' height='400' />
+      </MainContent>
+    </HeroSection>
+  );
 };
 
 export default index;
